cf0cc23d639c971ebd5afbf7147f8336df39cce3
Artistic18 <hrishitbiswas20@gmail.com>
2020-09-25 02:44:15 +0530

    changed epochs

--- music.py
+++ music.py
@@ -24,57 +24,57 @@ if parts:
     note = parts.parts[0].recurse()
 else:
     note = test.flat.notes
-    
+
 for element in note:
     print(element)
-    
+
 def train_network():
     notes = get_data()
     n_vocab = len(set(notes))
     network_ip, network_op = input_sequence(notes, n_vocab)
     model = create_network(network_ip, n_vocab)
     train(model, network_ip, network_op)
-    
+
 def get_data():
     notes = []
     for song in glob.glob("midi_songs/*.mid"):
         midi = converter.parse(song)
         print("Parsing %s" % song)
         notes_to_parse = None
-        
-        try: 
+
+        try:
             s2 = instrument.partitionByInstrument(midi)
-            notes_to_parse = s2.parts[0].recurse() 
-        except: 
+            notes_to_parse = s2.parts[0].recurse()
+        except:
             notes_to_parse = midi.flat.notes
-        
+
         for element in notes_to_parse:
             if isinstance(element, note.Note):
                 notes.append(str(element.pitch))
             elif isinstance(element, chord.Chord):
                 notes.append('.'.join(str(n) for n in element.normalOrder))
-      
+
     return notes
 
 def input_sequence(notes,n_vocab):
     sequence_length = 100
     pitchnames = sorted(set(i for i in notes))
-    
+
     note_to_int = dict((note, number) for number, note in enumerate(pitchnames))
     network_ip = []
     network_op = []
-    
+
     for i in range(0, len(notes) - sequence_length, 1):
         sequence_in = notes[i:i + sequence_length]
         sequence_out = notes[i + sequence_length]
         network_ip.append([note_to_int[char] for char in sequence_in])
         network_op.append(note_to_int[sequence_out])
     n_patterns = len(network_ip)
-        
+
     network_ip = numpy.reshape(network_ip, (n_patterns,sequence_length,1))
     network_ip = network_ip / float(n_vocab)
     network_op = np_utils.to_categorical(network_op)
-    
+
     return (network_ip,network_op)
 
 def create_network(network_ip, n_vocab):
@@ -95,7 +95,7 @@ def create_network(network_ip, n_vocab):
     model.add(Dense(n_vocab))
     model.add(Activation('softmax'))
     model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
-    
+
     return model
 
 def train(model, network_ip, network_op):
@@ -108,12 +108,7 @@ def train(model, network_ip, network_op):
             mode='min'
             )
     callbacks_list = [checkpoint]
-    model.fit(network_ip, network_op, epochs=200, batch_size=128, callbacks=callbacks_list)
+    model.fit(network_ip, network_op, epochs=300, batch_size=128, callbacks=callbacks_list)
 
-if __name__ == '__main__':   
-   train_network()   
-    
-    
-        
-    
-       
+if __name__ == '__main__':
+   train_network()
